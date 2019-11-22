0960829388933447daae74a2874c21c8a345ca46
leejh1230 <leejh4347@gmail.com>
2019-11-22 09:02:44 +0000

    chatbot

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const { RTMClient } = require('@slack/rtm-api');
+
+const token = process.env.SLACK_TOKEN;
+
+const rtm = new RTMClient(token);
+rtm.start();
+
+const food = require('./food');
+const movie = require('./movie');
+
+rtm.on('message', (message) => {
+  const { channel } = message;
+  const { text } = message;
+  switch (text) {
+    case '영화':
+      movie(rtm, channel);
+      break;
+    case '밥':
+      food(rtm, channel);
+      break;
+    case '놀이':
+      rtm.sendMessage('고만해.', channel);
+      break;
+    default:
+      rtm.sendMessage('안녕하세요. 영화, 밥, 놀이 중에 말씀해주세요.', channel);
+  }
+});
